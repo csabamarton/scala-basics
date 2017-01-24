4f32e1ac55181b4e07bc4993cc5779c3fc217929
Csaba Marton <martoncsab@gmail.com>
2017-01-24 01:09:44 +0100

    self type

--- src/main/scala/draft/WriterInherintanceTest.scala
+++ src/main/scala/draft/WriterInherintanceTest.scala
@@ -29,12 +29,24 @@ trait BinaryWriter extends BasedWriter {
   override def write(number: Int): Unit = super.write(number.toBinaryString.toInt)
 }
 
+trait WriteUser {
+  this: BasedWriter =>
+  def useWrite(data: Iterator[Int]): Unit = data.foreach(write)
+
+}
+
 object WriterInherintanceTest extends App {
-    val bufBinPrint = new SimpleWriter with BinaryWriter with BufferWriter
-    bufBinPrint.write(1)
-    bufBinPrint.write(2)
-    bufBinPrint.write(3)
-    bufBinPrint.write(4)
-    bufBinPrint.write(5)
-    bufBinPrint.write(6)
+  val bufBinPrint = new SimpleWriter with BinaryWriter with BufferWriter
+  bufBinPrint.write(1)
+  bufBinPrint.write(2)
+  bufBinPrint.write(3)
+  bufBinPrint.write(4)
+  bufBinPrint.write(5)
+  bufBinPrint.write(6)
+
+  val userWriter = new WriteUser with SimpleWriter with BufferWriter
+
+
+  userWriter.useWrite(List(1,2,3,4,5,6).iterator)
+
 }
